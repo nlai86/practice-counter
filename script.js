91ac77d27cc82ddafe6e21a16332b06361d408e8
nlai <nicholaslhlai@gmail.com>
2024-01-09 00:32:33 -0500

    Added from the top section

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let barNum = 0;
 let countNum = 0;
+let fttCount = 0; 
 
 function incBar() {
     barNum += 1;
@@ -36,6 +37,25 @@ function decCount() {
     document.getElementById("num-count").innerText = "Count: " + countNum;
 }
 
+function incCountFTT() {
+    fttCount += 1;
+
+    if (fttCount == 11) {
+        fttCount = 0;
+    }
+    document.getElementById("ftt-count").innerText = "From the Top Count: " + fttCount;
+}
+
+function decCountFTT() {
+    fttCount -= 1;
+
+    if (fttCount < 0) {
+        fttCount = 0;
+    }
+
+    document.getElementById("ftt-count").innerText = "From the Top Count: " + fttCount;
+}
+
 function reset(){
     countNum = 0;
     document.getElementById("num-count").innerText = "Count: " + countNum;
@@ -47,4 +67,6 @@ document.getElementById("next-button").addEventListener("click", incBar);
 document.getElementById("prev-button").addEventListener("click", decBar);
 document.getElementById("plus-one").addEventListener("click", incCount);
 document.getElementById("minus-one").addEventListener("click", decCount);
+document.getElementById("ftt-plus-one").addEventListener("click", incCountFTT);
+document.getElementById("ftt-minus-one").addEventListener("click", decCountFTT);
 document.getElementById("reset").addEventListener("click", reset);
